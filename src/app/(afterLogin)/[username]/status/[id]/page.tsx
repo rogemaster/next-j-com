aa1496fc6759623915a5410e70e380bf0e51b5ae
rogemaster <rlaaudwn000@naver.com>
2024-07-03 17:54:39 +0900

    섹션1 완료

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "../../../../page.module.css";
+import styles from "../../../../(beforeLogin)/_component/main.module.css";
 
 export default function Home() {
   return (
